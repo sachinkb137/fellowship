c835ee0c2a3ad4153b9e7dfe834d9c448b43fafb
Sachin K B <1si22cs150@sit.ac.in>
2025-11-01 12:08:05 +0530

    deployment errors5

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,9 +16,6 @@ export default defineConfig({
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
     }),
 
     VitePWA({
